ec577d1aafd8f9046e37152e3d8ed759a4b32d03
Roman Inozemtsev <roman.inozemtsev@gmail.com>
2014-06-09 00:28:39 -0700

    task #2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## this module has 2 function for cache solve(matrix)
 
-## Write a short comment describing this function
+## create a wrapper around matrix with cache inside.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(slv) s <<- slv
+  getsolve <- function() s
+  list(set = set,
+       get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## if cache is not set - solves the matrix and caches the result, if result is in the cache - just return 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  s <- x$getsolve()
+  if (!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data)
+  x$setsolve(s)
+  s
 }
+
+## some testing code
+
+test <- function() {
+  mat = matrix(c(3, 3.5, 3.2, 3.6), nrow=2, ncol=2)
+  m1 <- makeCacheMatrix(x = mat)
+  m1s <- cacheSolve(m1)
+  m1s2 <- cacheSolve(m1)
+}
+
+# t <- test()
